64e0b8f444bf6e17e0a092b13f0988ee5b574e62
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-05-27 22:13:55 +0100

    created a main fuctions to get my values

---
+++ Convert.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() {
+    val inputer = Scanner(System.`in`)
+
+    println("Please enter the value you want to convert:")
+    val input = inputer.nextDouble()
+
+    println("Please enter the current unit of value you just input:")
+    val valueUnit = inputer.next()
+
+    println("Please enter the unit of measurement to convert to the value to:")
+    val convetionUnit = inputer.next()
+
+}
+
+
