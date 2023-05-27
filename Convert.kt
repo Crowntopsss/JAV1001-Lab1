4889ef32cd044fa0389ced88187689cbaefb40d0
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-05-27 22:15:05 +0100

    Create the conversion function

--- Convert.kt
+++ Convert.kt
@@ -12,6 +12,18 @@ fun main() {
     println("Please enter the unit of measurement to convert to the value to:")
     val convetionUnit = inputer.next()
 
+    val answer = convertValue(input, valueUnit, convetionUnit)
+    println("This is the result: $answer")
 }
 
-
+fun convertValue(input: Double, valueUnit: String, convetionUnit: String): Double {
+    return when (valueUnit.toLowerCase() to convetionUnit.toLowerCase()) {
+        "km" to "mi" -> input * 0.62
+        "mi" to "km" -> input * 1.61
+        "cm" to "in" -> input * 0.39
+        "in" to "cm" -> input * 2.54
+        "kg" to "lb" -> input * 2.2
+        "lb" to "kg" -> input * 0.4
+        else -> input 
+    }
+}
